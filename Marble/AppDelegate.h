d2b562d326596b477f27839485f68af6d2781aa7
Iru <iru@Irus-Mac.local>
2014-09-29 13:41:18 -0700

    Initial Commit

---
+++ Marble/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Marble
+//
+//  Created by Iru on 9/29/14.
+//  Copyright (c) 2014 Orrzs Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Marble/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Marble
+//
+//  Created by Iru on 9/29/14.
+//  Copyright (c) 2014 Orrzs Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
