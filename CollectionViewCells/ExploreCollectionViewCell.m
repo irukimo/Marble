b21092f0b00d3d004cfccb8a5bb0b6a168f1a479
Iru <iru@Irus-Mac.local>
2014-11-10 13:52:29 -0800

    Fix explorecollectionviewcell to have number topright and name to bottom

--- CollectionViewCells/ExploreCollectionViewCell.m
+++ CollectionViewCells/ExploreCollectionViewCell.m
@@ -32,6 +32,7 @@
 
 -(void) prepareForReuse{
     [_keywordLabel setText:@""];
+    [_nameLabel setFrame:[self getNameNormalFrame]];
 }
 
 
@@ -51,7 +52,7 @@
 }
 
 -(void) initStaticLabels{
-    _nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(5, 80, self.contentView.frame.size.width, 20)];
+    _nameLabel = [[UILabel alloc] initWithFrame:[self getNameNormalFrame]];
     _nameLabel.layer.shadowColor = [UIColor blackColor].CGColor;
     _nameLabel.layer.shadowRadius = 3.f;
     _nameLabel.layer.shadowOpacity = 1.f;
@@ -60,12 +61,20 @@
     [self.contentView addSubview:_nameLabel];
     [self.contentView addSubview:_keywordLabel];
     
-    UIImageView *marbleView = [[UIImageView alloc] initWithFrame:CGRectMake(self.contentView.frame.size.width - 40, self.contentView.frame.size.width - 25, 20, 20)];
+    UIImageView *marbleView = [[UIImageView alloc] initWithFrame:CGRectMake(self.contentView.frame.size.width - 40, 5, 20, 20)];
     [marbleView setImage:[UIImage imageNamed:MARBLE_IMAGE_NAME]];
     [self.contentView addSubview:marbleView];
     
-    _receiveNumLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.contentView.frame.size.width - 20, self.contentView.frame.size.width - 25, 20, 20)];
+    _receiveNumLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.contentView.frame.size.width - 20, 5, 20, 20)];
+    _receiveNumLabel.layer.shadowColor = [UIColor blackColor].CGColor;
+    _receiveNumLabel.layer.shadowRadius = 3.f;
+    _receiveNumLabel.layer.shadowOpacity = 1.f;
+    _receiveNumLabel.layer.shadowOffset = CGSizeMake(0, 0);
     [self.contentView addSubview:_receiveNumLabel];
+}
+
+-(CGRect)getNameNormalFrame{
+    return CGRectMake(5, 88, self.contentView.frame.size.width, 20);
 }
 
 -(void)setCellUser:(User *)user{
@@ -110,6 +119,10 @@
                 [_keywordLabel setAttributedText:keywordString];
             }
         }
+    }else{
+        CGRect nameFrame = [self getNameNormalFrame];
+        nameFrame.origin.y = nameFrame.origin.y + 12;
+        [_nameLabel setFrame:nameFrame];
     }
 //    NSString *authorPictureUrl = [NSString stringWithFormat:@"https://graph.facebook.com/%@/picture?width=%lu&height=%lu", _user.fbID,(unsigned long)self.contentView.frame.size.width*2 , (unsigned long)self.contentView.frame.size.height*2 ];
 //    [_authorPicView setImageWithURL:[NSURL URLWithString:authorPictureUrl] placeholderImage:[UIImage imageNamed:@"login.png"]];
